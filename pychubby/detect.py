efa35007578f0935daf0f5f957ff023be3b1bce7
Jan Krepl <jankrepl@yahoo.com>
2019-07-14 21:48:53 +0200

    implement dlib 68 landmarks wrapper

--- pychubby/detect.py
+++ pychubby/detect.py
@@ -1,8 +1,11 @@
 """Collection of detection algorithms."""
+import pathlib
 
 import dlib
 import numpy as np
 from skimage.util import img_as_ubyte
+
+from pychubby.base import CACHE_FOLDER
 
 
 def face_rectangle(img):
@@ -39,3 +42,44 @@ def face_rectangle(img):
         corners.append((top_left, bottom_right))
 
     return corners, faces
+
+
+def landmarks_68(img, rectangle, model_path=None):
+    """Predict 68 face landmarks.
+
+    Parameters
+    ----------
+    img : np.ndarray
+        Image of any dtype and number of channels.
+
+    rectangle : dlib.rectangle
+        Rectangle that represents the bounding box around a single face.
+
+    model_path : str or pathlib.Path, default=None
+        Path to where the pretrained model is located. If None then using the `CACHE_FOLDER` model.
+
+    Returns
+    -------
+    lm_points : np.ndarray
+        Array of shape `(68, 2)` where rows are different landmark points and the columns
+        are x and y coordinates.
+
+    original : dlib.full_object_detection
+        Instance of ``dlib.full_object_detection``.
+
+    """
+    if model_path is None:
+        model_path = CACHE_FOLDER / 'shape_predictor_68_face_landmarks.dat'
+    else:
+        model_path = pathlib.Path(model_path)
+
+    if not model_path.is_file():
+        raise IOError('Invalid landmark model, {}'.format(str(model_path)))
+
+    lm_predictor = dlib.shape_predictor(str(model_path))
+
+    original = lm_predictor(img_as_ubyte(img), rectangle)
+
+    lm_points = np.array([[p.x, p.y] for p in original.parts()])
+
+    return lm_points, original
--- tests/test_detect.py
+++ tests/test_detect.py
@@ -1,10 +1,13 @@
 """Collection of tests focused on the `detect` module."""
+from collections import namedtuple
+from unittest.mock import Mock
 
+import dlib
 import numpy as np
 import pytest
 
-from pychubby.detect import face_rectangle
-
+import pychubby.detect
+from pychubby.detect import face_rectangle, landmarks_68
 
 class TestFaceRectangle:
     """Tests of the `face_rectangle` function."""
@@ -27,3 +30,38 @@ class TestFaceRectangle:
         res, _ = face_rectangle(faces_img)
         assert isinstance(res, list)
         assert len(res) == 2
+
+
+class TestLandmarks68:
+    """Tests of the `landmarks_68` function."""
+    @pytest.fixture()
+    def dlib_rectangle(self):
+        return Mock(spec=dlib.rectangle) 
+
+    def test_incorrect_input_model(self, face_img, dlib_rectangle, tmp_path, monkeypatch):
+        with pytest.raises(IOError):
+            lm_points, original = landmarks_68(face_img, dlib_rectangle, tmp_path / 'fake_model.dat')
+        # nasty hack
+        monkeypatch.setattr('pychubby.detect.CACHE_FOLDER', tmp_path)
+        with pytest.raises(IOError):
+            lm_points, original = pychubby.detect.landmarks_68(face_img, dlib_rectangle)
+
+    def test_correct_output(self, face_img, dlib_rectangle, tmp_path, monkeypatch):
+
+        fake_model = tmp_path / 'fake_model.dat'
+        fake_model.touch()
+
+        def fake_shape_predictor(model_path):
+            trained_model = Mock()
+            Point = namedtuple('Point', ['x', 'y'])
+            trained_model.parts = Mock(return_value=68 * [Point(0, 0)])
+            dlib_predictor = Mock(return_value=trained_model)
+
+            return dlib_predictor
+        
+        monkeypatch.setattr('dlib.shape_predictor', fake_shape_predictor)
+
+        lm_points, original = landmarks_68(face_img, dlib_rectangle, fake_model)
+        
+        assert isinstance(lm_points, np.ndarray)
+        assert lm_points.shape == (68, 2)
