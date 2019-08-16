1802b550156c404dbbcda4cddfa991c833adfd78
Jan Krepl <jankrepl@yahoo.com>
2019-08-16 17:35:17 +0200

    add initial docs framework

---
+++ docs/index.rst
@@ -0,0 +1,25 @@
+PyChubby
+========
+
+`pychubby` is a package for automated face warping. It allows the user to programatically
+change facial expressions and shapes of any person in an image.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   source/installation
+   source/basic_example
+   source/pipeline_example
+   source/multiple_example
+   source/gallery
+   source/adding_custom_actions
+   source/api
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup
 
-setup(name='pychubby',
-      version=0.1,
-      packages=['pychubby'],
-      install_requires=[
-                        'click>=7.0',
-                        'dlib',
-                        'matplotlib>=2.0.0',
-                        'numpy>=1.16.4',
-                        'opencv-python>=4.1.0.25',
-                        'scikit-image' 
-                        ],
-      extras_require={'dev': [
-                              'codecov',
-                              'flake8',
-                              'pytest>=3.6',
-                              'pytest-cov',
-                              'tox'
-                              ]
-                              },
-      entry_points={'console_scripts': ['pc = pychubby.cli:cli']}
-      )
+setup(
+    name="pychubby",
+    version=0.1,
+    packages=["pychubby"],
+    install_requires=[
+        "click>=7.0",
+        "dlib",
+        "matplotlib>=2.0.0",
+        "numpy>=1.16.4",
+        "opencv-python>=4.1.0.25",
+        "scikit-image",
+    ],
+    extras_require={
+        "dev": ["codecov", "flake8", "pytest>=3.6", "pytest-cov", "tox"],
+        "docs": ["sphinx", "sphinx_rtd_theme"],
+    },
+    entry_points={"console_scripts": ["pc = pychubby.cli:cli"]},
+)
