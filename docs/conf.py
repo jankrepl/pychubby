f94275e5da80ad09d0b4d648bd50fa6d75d38eec
Jan Krepl <jankrepl@yahoo.com>
2019-09-09 23:53:43 +0200

    add dlib mocker for rtd

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Mocking
+autodoc_mock_imports = ["dlib"]
 
 # -- Options for HTML output -------------------------------------------------
 
