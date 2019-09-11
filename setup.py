26e1b37cda38d7732d8c51052ff17d963d9d871f
Jan Krepl <jankrepl@yahoo.com>
2019-09-11 14:46:38 +0200

    exclude dlib from setup.py for readthedocs
    trying a different key

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
+import os
 
 import pychubby
 
+INSTALL_REQUIRES = [
+    "click>=7.0",
+    "matplotlib>=2.0.0",
+    "numpy>=1.16.4",
+    "opencv-python>=4.1.0.25",
+    "scikit-image",
+]
+
+if "RTD_BUILD" not in os.environ:
+    # ReadTheDocs cannot handle compilation
+    INSTALL_REQUIRES += ["dlib"]
+
 LONG_DESCRIPTION = "Automated face warping tool"
 PROJECT_URLS = {
-    'Bug Tracker': 'https://github.com/jankrepl/pychubby/issues',
-    'Documentation': 'https://pychubby.readthedocs.io',
-    'Source Code': 'https://github.com/jankrepl/pychubby'
+    "Bug Tracker": "https://github.com/jankrepl/pychubby/issues",
+    "Documentation": "https://pychubby.readthedocs.io",
+    "Source Code": "https://github.com/jankrepl/pychubby",
 }
 VERSION = pychubby.__version__
 
@@ -18,7 +31,7 @@ setup(
     description="Automated face warping tool",
     long_description=LONG_DESCRIPTION,
     url="https://github.com/jankrepl/pychubby",
-	project_urls=PROJECT_URLS,
+    project_urls=PROJECT_URLS,
     packages=["pychubby"],
     license="MIT",
     classifiers=[
@@ -39,17 +52,10 @@ setup(
         "Programming Language :: Python :: 3.7",
         ("Programming Language :: Python :: " "Implementation :: CPython"),
     ],
-    install_requires=[
-        "click>=7.0",
-        "dlib",
-        "matplotlib>=2.0.0",
-        "numpy>=1.16.4",
-        "opencv-python>=4.1.0.25",
-        "scikit-image",
-    ],
+    install_requires=INSTALL_REQUIRES,
     extras_require={
         "dev": ["codecov", "flake8", "pytest>=3.6", "pytest-cov", "tox"],
         "docs": ["sphinx", "sphinx_rtd_theme"],
     }
-    #entry_points={"console_scripts": ["pc = pychubby.cli:cli"]},
+    # entry_points={"console_scripts": ["pc = pychubby.cli:cli"]},
 )
